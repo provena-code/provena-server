6812953b476b1b2a4a20a21a8acccd18b109dec7
Thomas Price <twprice@ncsu.edu>
2025-12-30 11:47:33 -0500

    Follow renames for event counting

--- src/provena/api/logging/logging.py
+++ src/provena/api/logging/logging.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from typing import List, Literal, Optional
 
 from pydantic import BaseModel, Field
-from sqlalchemy import func, select
+from sqlalchemy import Column, Connection, Table, func, select
 
 from progsnap2.api.config import PS2APIConfig
 from progsnap2.api.models import TempCodeStateEntry
@@ -50,6 +50,8 @@ def add_events_with_code_states(events: List[MainTableEvent], writer: SQLWriter 
 
 class CodeStateSection(BaseModel):
     CodeStateSection: str
+    # TODO: The code should probably be optional, since we
+    # don't need it for just event checks
     Code: str
 
 class SubmissionInfo(BaseModel):
@@ -108,32 +110,39 @@ def log_submit(event: SubmitEvent, writer: SQLWriter = Depends(create_writer)): 
     print(f"Logging {len(events)} events", events)
     return writer.add_events(events)
 
-@router.post("/submit_and_count", operation_id="submitAndGetCount")
-def log_submit_and_get_count(event: SubmitEvent, writer: SQLWriter = Depends(create_writer)): # type: ignore
-    try:
-        result = log_submit(event, writer)
-        if (not result.success):
-            print("Error logging event:", result.errors)
-    except Exception as e:
-        print(f"Error logging event: {e}")
-        pass
-
-    count = get_event_count(event, writer)
-    return count
-
 @router.post("/get_event_count", operation_id="getEventCount")
 def get_event_count(info: SubmissionInfo, writer: SQLWriter = Depends(create_writer)): # type: ignore
     manager = writer.context.table_manager
-    codestates_table = manager.get_table(CoreTables.CodeStates)
     main_table = manager.get_table(CoreTables.MainTable)
-    codestate_sections = [section.CodeStateSection for section in info.CodeState]
+    codestate_sections = [cs.CodeStateSection for cs in info.CodeState]
+    result = get_event_count_for_codestate(writer.conn, main_table, info.SubjectIDs, codestate_sections)
+    return result
+
+def get_event_count_for_codestate(conn: Connection, main_table: Table, subject_ids: List[str], codestate_sections: List[str], alread_checked_codestate_sections: set[str] = set()) -> int:
     # TODO: Also confirm that the code being submitted has logs
-    # TODO: Also check for renames
     statement = select(func.count()).where(
-        main_table.c.SubjectID.in_(info.SubjectIDs),
+        main_table.c.SubjectID.in_(subject_ids),
         main_table.c.CodeStateSection.in_(codestate_sections)
     )
-    result = writer.conn.execute(statement).scalar()
+    result = conn.execute(statement).scalar()
+
+    def c(col: str) -> Column:
+        return main_table.c[col]
+
+    alread_checked_codestate_sections.update(codestate_sections)
+
+    # Check for any other names these codestate sections have had before renames
+    other_file_names = select(c(Cols.CodeStateSection).distinct()).where(
+        c(Cols.SubjectID).in_(subject_ids),
+        c(Cols.DestinationCodeStateSection).in_(codestate_sections)
+    )
+    other_sections = [row[0] for row in conn.execute(other_file_names).fetchall()]
+    # print("Other sections to check:", other_sections)
+    other_sections_to_check = [s for s in other_sections if s not in alread_checked_codestate_sections]
+
+    if len(other_sections_to_check) > 0:
+        result += get_event_count_for_codestate(conn, main_table, subject_ids, other_sections_to_check, alread_checked_codestate_sections)
+
     return result
 
 
